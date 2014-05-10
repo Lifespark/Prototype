c6929968ed92cdf09a9c7e5bf254f5dfeb918da8
Steven Li <listeven@usc.edu>
2014-05-09 23:23:00 -0700

    Revert "Boss movement"
    This reverts commit 0cd36641446bc79c91310b88b80f1df68f393e7f.

--- LifeSpark/Assets/_Scripts/Boss.cs
+++ LifeSpark/Assets/_Scripts/Boss.cs
@@ -3,9 +3,7 @@ using System.Collections;
 
 public class Boss : LivingObject {
     //public int health = 100;
-	LayerMask mask;
-	Vector3 goal;
-	public float speed = 2;
+    
     // Use this for initialization
     void Start() {
         health = 100;
@@ -16,8 +14,6 @@ public class Boss : LivingObject {
             return;
         GUIManager.guiManager.addHealthBar(this);
         isInited = true;
-		mask = ~1<<12;
-		randomizeGoal();
     }
 
     // Update is called once per frame
@@ -25,37 +21,7 @@ public class Boss : LivingObject {
         if (onHealthChanged != null) {
             onHealthChanged(health);
         }
-		if(Network.isServer){
-			if(Random.Range(0,100)<1)
-				randomizeGoal();
-			while(Physics.Raycast(new Vector3(transform.position.x,.5f,transform.position.z),goal,100)){
-				Debug.Log("haps");
-				randomizeGoal();
-			}
-		}
-		transform.Translate(goal*speed*Time.deltaTime);
-		
     }
-
-	void randomizeGoal(){
-		float xDir = Random.Range(-100,100)/100.0f;
-		float yDir = Random.Range(-100,100)/100.0f;
-		networkView.RPC("UpdateGoal",RPCMode.All,new Vector3(xDir,0,yDir).normalized);
-	}
-
-	void OnCollisionEnter(Collision col){
-		if(Network.isServer)
-		if(col.gameObject.name!="Ground"&&!col.gameObject.name.StartsWith("Lane")){
-			randomizeGoal();
-		}
-	}
-
-	void OnCollisionStay(Collision col){
-		if(Network.isServer)
-		if(col.gameObject.name!="Ground"&&!col.gameObject.name.StartsWith("Lane")){
-			randomizeGoal();
-		}
-	}
 
     [RPC]
     void ApplyDamage(int pDamage) {
@@ -66,13 +32,7 @@ public class Boss : LivingObject {
 
     }
 
-	[RPC]
-	void UpdateGoal(Vector3 g){
-		goal = g;
-	}
-
     void OnGUI() {
         GUI.Label(new Rect(10, 10, 100, 50), "BOSS health: " + health);
     }
-
 }
